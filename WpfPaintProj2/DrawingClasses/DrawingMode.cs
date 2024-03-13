7288db3bcbb4f92de5ca2418401f59352034e5fc
Ilya Novikov <ilyanovickov@mail.ru>
2024-03-13 20:52:51 +0300

    .

---
+++ WpfPaintProj2/DrawingClasses/DrawingMode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPaintProj2.DrawingClasses
+{
+    public enum DrawingMode
+    {
+        None,
+        Dragging,
+        Resizing
+    }
+}
---
+++ WpfPaintProj2/Helpers/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfPaintProj2.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object> action;
+        private Predicate<object> predicate;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            this.action = execute;
+            this.predicate = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.predicate == null || this.predicate(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.action(parameter);
+        }
+    }
+}
